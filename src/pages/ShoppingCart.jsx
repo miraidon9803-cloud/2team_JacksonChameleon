b74997ee13d2d05e90385eb0ed742b10a22589a5
CheonU <96rhysc@gmail.com>
2025-11-20 18:43:35 +0900

    1120 기타등등

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -71,65 +71,73 @@ const ShoppingCart = () => {
                                 <p>장바구니가 비어있습니다.</p>
                             </div>
                         ) : (
-                            cartItems.map((item, index) => (
+                            <div className="item-list">
+                                {
+                                    cartItems.map((item, index) => (
 
-                                <div className="item-wrap" key={index}>
-                                    <div className="selected-wrap">
-                                        <input
-                                            type="checkbox"
-                                            checked={selectedItems.includes(item)}
-                                            onChange={() => handleSelectItem(item)}
-                                        />
-                                        <button className='close-btn' onClick={() => onRemoveCart(item)}><img src="/images/close-icon.png" alt="삭제버튼" /></button>
-                                    </div>
-
-                                    <div className="item-box">
-                                        <div className="item-img"><img src={item.size.img} alt={item.title} /></div>
-
-                                        <div className="item-info">
-                                            <h4 className='item-title'>{item.title}</h4>
-                                            <p className='item-option'>{item.sheet.text} / {item.size.sizename} / {item.color.colorname} / {item.add ? item.add.cushion : '선택안함'}
-                                            </p>
-                                            <button className='btn-option'><span>옵션변경</span></button>
-                                        </div>
-
-                                        <div className="count-price-wrap">
-                                            <div className="count-wrap">
-                                                <button onClick={() => onItemMinus(item)}><img src="/images/minus.png" alt="빼기아이콘" /></button>
-                                                <span>{item.qty}</span>
-                                                <button onClick={() => onItemPlus(item)}><img src="/images/plus.png" alt="더하기아이콘" /></button>
+                                        <div className="item-wrap" key={index}>
+                                            <div className="selected-wrap">
+                                                <input
+                                                    type="checkbox"
+                                                    checked={selectedItems.includes(item)}
+                                                    onChange={() => handleSelectItem(item)}
+                                                />
+                                                <button className='close-btn' onClick={() => onRemoveCart(item)}><img src="/images/close-icon.png" alt="삭제버튼" /></button>
                                             </div>
-                                            <p className='price'>
-                                                {/* {totalPrice.toLocaleString()}원 */}
-                                                {getItemTotal(item).toLocaleString()}원
 
-                                            </p>
+                                            <div className="item-box">
+                                                <div className="item-img"><img src={item.size.img} alt={item.title} /></div>
+
+                                                <div className="item-sel">
+                                                    <div className="item-info">
+                                                        <h4 className='item-title'>{item.title}</h4>
+                                                        <p className='item-option'>{item.sheet.text} / {item.size.sizename} / {item.color.colorname} / {item.add ? item.add.cushion : '선택안함'}
+                                                        </p>
+                                                        <button className='btn-option'><span>옵션변경</span></button>
+                                                    </div>
+
+
+                                                    <div className="count-wrap">
+                                                        <button onClick={() => onItemMinus(item)}><img src="/images/minus.png" alt="빼기아이콘" /></button>
+                                                        <span>{item.qty}</span>
+                                                        <button onClick={() => onItemPlus(item)}><img src="/images/plus.png" alt="더하기아이콘" /></button>
+                                                    </div>
+                                                    <p className='price'>
+                                                        {/* {totalPrice.toLocaleString()}원 */}
+                                                        {getItemTotal(item).toLocaleString()}원
+
+                                                    </p>
+
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            ))
-                        )}
+                                    ))
+                                }
+                            </div>
+                        )
+                        }
                     </div>
 
-                    <div className="total-wrap">
-                        <div className="total-content">
-                            <h4>구매 금액</h4>
-                            <ul>
-                                <li><span>상품금액</span><span>{totalPrice.toLocaleString()}원</span></li>
-                                <li><span>할인 금액</span><span>143,000원</span></li>
-                                <li><span>적립금</span><span>7,672</span></li>
-                                <li><span>배송비</span><span>무료배송</span></li>
-                            </ul>
+                    <div className="total">
+                        <div className="total-wrap">
+                            <div className="total-content">
+                                <h4>구매 금액</h4>
+                                <ul>
+                                    <li><span>상품금액</span><span>{totalPrice.toLocaleString()}원</span></li>
+                                    <li><span>할인 금액</span><span>143,000원</span></li>
+                                    <li><span>적립금</span><span>7,672</span></li>
+                                    <li><span>배송비</span><span>무료배송</span></li>
+                                </ul>
 
-                            <div className="total-price">
-                                <span>총 구매 금액</span>
-                                <strong>{totalPrice.toLocaleString()}원</strong>
+                                <div className="total-price">
+                                    <span>총 구매 금액</span>
+                                    <strong>{totalPrice.toLocaleString()}원</strong>
+                                </div>
+
+                                <button className='pay-btn'><span>결제하기</span></button>
                             </div>
-
-                            <button className='pay-btn'><span>결제하기</span></button>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div >
