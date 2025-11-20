5cefc7396a909506bce9124081177f5fdfc86a23
CheonU <96rhysc@gmail.com>
2025-11-20 12:23:42 +0900

    1120 payment 제작

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,8 +2,25 @@ import React, { useState } from 'react'
 import './scss/Checkout.scss'
 
 const Checkout = () => {
+
+  const [isReqOpen, setIsReqeqOpen] = useState('');
+  const [isCustomInput, setIsCustomInput] = useState(false);
+  const [reqText, setReqText] = useState('');
+  const reqOptions = [
+    { id: 'opt1', label: '부재 시 경비실에 맡겨주세요', type: 'preset' },
+    { id: 'opt2', label: '부재 시 택배함에 놓아주세요', type: 'preset' },
+    { id: 'opt3', label: '배송 전에 연락 부탁드립니다', type: 'preset' },
+  ];
+
+
   const [selectedMethod, setSelectedMethod] = useState('');
   const [selectedMethodBtn, setSelectedMethodBtn] = useState(null);
+  const simpleOpt = [
+    { id: 'naver', label: '네이버페이', img: '/images/pay-naver.png', activeimg: '/images/pay-naver-active.png' },
+    { id: 'kakao', label: '카카오페이', img: '/images/pay-kakao.png', activeimg: '/images/pay-kakao-active.png' },
+    { id: 'samsung', label: '삼성페이', img: '/images/pay-samsung.png', activeimg: '/images/pay-samsung-active.png' },
+    { id: 'toss', label: '토스페이', img: '/images/pay-toss.png', activeimg: '/images/pay-toss-active.png' }
+  ]
 
 
   return (
@@ -27,14 +44,97 @@ const Checkout = () => {
 
             <div className="left-con2 req">
               <p>요청사항</p>
-              <div className="req-list">직접 입력하기</div>
+
+              <div className='req-list-wrap'
+                onClick={() => setIsReqeqOpen(prev => !prev)}>
+                <div className="req-list">
+                  {isCustomInput ? (
+                    <input
+                      type='text'
+                      value={reqText}
+                      autoFocus
+                      placeholder='요청사항을 입력해 주세요'
+                      onChange={(e) => setReqText(e.target.value)}
+                      onClick={(e) => e.stopPropagation()} />
+                  ) : (
+                    <p className='req-selected-text'>
+                      {reqText || '요청사항을 선택해 주세요'}
+                    </p>
+                  )
+                  }
+
+                  <p className={`req-arrow ${isReqOpen ? 'active' : ''}`}>
+                    <img src="/images/Arrow-down.png" alt="" />
+                  </p>
+                </div>
+              </div>
+
+              {isReqOpen && (
+                <div className="req-dropdown">
+
+                  {reqOptions.map((opt) => (
+                    <div
+                      className={`req-item ${reqText === opt.label ? 'selected' : ''}`}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setIsCustomInput(false);
+                        setReqText(opt.label);
+                        setIsReqeqOpen(false);
+                      }}>
+                      {opt.label}
+                    </div>
+                  ))}
+
+                  <div className="req-item"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setIsCustomInput(true);
+                      setReqText('');
+                      setIsReqeqOpen(false);
+                    }}>
+                    요청사항 직접 입력하기
+                  </div>
+                </div>
+
+              )}
+
+
             </div>
 
             <div className="left-con3 order">
               <p>주문상품</p>
 
-              <div>
-
+              <div className='order-item'>
+                <div className="item-img"><img src="/images/sofa-1.png" alt="" /></div>
+                <div className="item-info">
+                  <p className="item-title">asdfsadf</p>
+                  <p className="item-option">asdfsadf</p>
+                  <p className="item-price">asdfsadf</p>
+                </div>
+              </div>
+              <div className='order-item'>
+                <div className="item-img"><img src="/images/sofa-1.png" alt="" /></div>
+                <div className="item-info">
+                  <p className="item-title">asdfsadf</p>
+                  <p className="item-option">asdfsadf</p>
+                  <p className="item-price">asdfsadf</p>
+                </div>
+              </div>
+              <div className='order-item'>
+                <div className="item-img"><img src="/images/sofa-1.png" alt="" /></div>
+                <div className="item-info">
+                  <p className="item-title">asdfsadf</p>
+                  <p className="item-option">asdfsadf</p>
+                  <p className="item-price">asdfsadf</p>
+                </div>
+              </div>
+              <div className='order-item'>
+                <div className="item-img"><img src="/images/sofa-1.png" alt="" /></div>
+                <div className="item-info">
+                  <p className="item-title">asdfsadf</p>
+                  <p className="item-option">asdfsadf</p>
+                  <p className="item-price">asdfsadf</p>
+                </div>
               </div>
             </div>
 
@@ -58,17 +158,19 @@ const Checkout = () => {
                   <input type="radio" name='method' value='simple' onChange={() => setSelectedMethod('simple')} />
                   간편결제
                   <div className={`selected-method ${selectedMethod === 'simple' ? 'active' : ''}`}>
-                    {['네이버', '카카오', '삼성', '토스'].map((btn) => (
+                    {simpleOpt.map((btn) => (
                       <button
                         type='button'
-                        key={btn}
-                        className={selectedMethodBtn === btn ? 'active' : ''}
-                        onClick={() => setSelectedMethodBtn(btn)}>{btn}</button>
+                        key={btn.id}
+                        className={selectedMethodBtn === btn.id ? 'active' : ''}
+                        onClick={() => setSelectedMethodBtn(btn.id)}
+                      >
+                        <img
+                          src={selectedMethodBtn === btn.id ? btn.activeimg : btn.img}
+                          alt={btn.label}
+                        />
+                      </button>
                     ))}
-                    {/* <button>네이버</button>
-                    <button>카카오</button>
-                    <button>삼성</button>
-                    <button>토스</button> */}
                   </div>
                 </label>
                 <label>
@@ -82,17 +184,34 @@ const Checkout = () => {
                         className={selectedMethodBtn === btn ? 'active' : ''}
                         onClick={() => setSelectedMethodBtn(btn)}>{btn}</button>
                     ))}
-                    {/* <button>무통장 입금</button>
-                    <button>카드결제</button>
-                    <button>가상계좌</button>
-                    <button>실시간 입금</button> */}
                   </div>
                 </label>
               </form>
             </div>
-
           </div>
-          <div className="right"></div>
+
+
+
+          <div className="right">
+            <div className="total-wrap">
+              <div className="total-content">
+                <h4>구매 금액</h4>
+                <ul>
+                  <li><span>상품금액</span><span>0000원</span></li>
+                  <li><span>할인 금액</span><span>143,000원</span></li>
+                  <li><span>적립금</span><span>7,672</span></li>
+                  <li><span>배송비</span><span>무료배송</span></li>
+                </ul>
+
+                <div className="total-price">
+                  <span>총 구매 금액</span>
+                  <strong>000원</strong>
+                </div>
+
+                <button className='pay-btn'><span>결제하기</span></button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
