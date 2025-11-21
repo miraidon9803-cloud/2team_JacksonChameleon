443aedc71904b7d53cc9f07ca1cfe18c5e7dd65f
alsrl980409 <miraidon9803@gmail.com>
2025-11-21 12:08:05 +0900

    쿠폰팝업완료

--- src/components/Coupon.jsx
+++ src/components/Coupon.jsx
@@ -10,13 +10,13 @@ const Coupon = ({ onClose }) => {
     <div className='coupon-wrap'>
       <div className="inner">
         <div className="coupon-title">
-        <h3>쿠폰사용</h3>
-        <button onClick={onClose}>x</button>
+          <h3>쿠폰사용</h3>
+          <p className='coupon-close' onClick={onClose}><img src="/images/close-icon.png" alt="" /></p>
         </div>
 
         <ul className="coupon-list">
           {coupons.map((c) => (
-            <li key={c.id} className="coupon-item">
+            <li key={c.id} className={`coupon-item ${selectedCoupon?.id === c.id ? "active" : ""}`}>
               <label>
                 <input
                   className='coupons'
@@ -25,18 +25,25 @@ const Coupon = ({ onClose }) => {
                   checked={selectedCoupon?.id === c.id}
                   onChange={() => onSelectCoupon(c)}
                 />
-                <div className="coupon-right">
-                  <p>{c.price.toLocaleString("ko-KR")}원 할인</p>
-                  <p>{c.text}</p>
-                  <p>2026년 1월 31일까지</p>
+
+                <div className="coupon-right active">
+                  <div className="coupon-title1">
+                    <div className="coupon-text1">
+                      <div className='coupon-text2'>
+                        <p className='disprice'>{c.price.toLocaleString("ko-KR")}원 할인</p>
+                        <p className='coupon-text'>{c.text}</p>
+                      </div>
+                      <p className='coupon-date'>2026년 1월 31일까지</p>
+                    </div>
+                  </div>
+                  <p className='coupon-true'>{c.status}</p>
                 </div>
-                <p>{c.status}</p>
               </label>
             </li>
           ))}
         </ul>
 
-        <p>적용하기</p>
+        <button className='con-btn'>적용하기</button>
       </div>
     </div>
   )
