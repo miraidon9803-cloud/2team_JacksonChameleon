27219fe398ddcfa005ffa5e6dec7a50e059c9663
CheonU <96rhysc@gmail.com>
2025-11-21 15:54:28 +0900

    1121쇼핑카트 수정

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -69,6 +69,7 @@ const ShoppingCart = () => {
                                     type="checkbox"
                                     checked={isAllSelected}
                                     onChange={handleSelectAll}
+                                    className='real'
                                 />
                                 <span className='fake'><img className='icon' src="/images/check.png" alt="checkicon" /></span>
                             </label>
@@ -80,67 +81,76 @@ const ShoppingCart = () => {
                                 <p>장바구니가 비어있습니다.</p>
                             </div>
                         ) : (
-                            cartItems.map((item, index) => (
+                            <div className="item-list">
+                                {
+                                    cartItems.map((item, index) => (
 
-                                <div className="item-wrap" key={index}>
-                                    <div className="selected-wrap">
-                                        <label className="check-box">
+                                        <div className="item-wrap" key={index}>
+                                            <div className="selected-wrap">
+                                                <label className="check-box">
 
-                                            <input
-                                                type="checkbox"
-                                                checked={item.checked}
-                                                onChange={() => handleSelectItem(item.cartId)}
-                                            />
-                                            <span className='fake'><img className='icon' src="/images/check.png" alt="checkicon" /></span>
-                                        </label>
-                                        <button className='close-btn' onClick={() => onRemoveCart(item)}><img src="/images/close-icon.png" alt="삭제버튼" /></button>
-                                    </div>
-
-                                    <div className="item-box">
-                                        <div className="item-img"><img src={item.size.img} alt={item.title} /></div>
-
-                                        <div className="item-info">
-                                            <h4 className='item-title'>{item.title}</h4>
-                                            <p className='item-option'>{item.sheet.text} / {item.size.sizename} / {item.color.colorname} / {item.add ? item.add.cushion : '선택안함'}
-                                            </p>
-                                            <button className='btn-option'><span>옵션변경</span></button>
-                                            <OptionChange item={item} />
-                                        </div>
-
-                                        <div className="count-price-wrap">
-                                            <div className="count-wrap">
-                                                <button onClick={() => onItemMinus(item)}><img src="/images/minus.png" alt="빼기아이콘" /></button>
-                                                <span>{item.qty}</span>
-                                                <button onClick={() => onItemPlus(item)}><img src="/images/plus.png" alt="더하기아이콘" /></button>
+                                                    <input
+                                                        type="checkbox"
+                                                        checked={item.checked}
+                                                        onChange={() => handleSelectItem(item.cartId)}
+                                                        className='real'
+                                                    />
+                                                    <span className='fake'><img className='icon' src="/images/check.png" alt="checkicon" /></span>
+                                                </label>
+                                                <button className='close-btn' onClick={() => onRemoveCart(item)}><img src="/images/close-icon.png" alt="삭제버튼" /></button>
                                             </div>
-                                            <p className='price'>
-                                                {getItemTotal(item).toLocaleString()}원
-                                            </p>
+
+                                            <div className="item-box">
+                                                <div className="item-img"><img src={item.size.img} alt={item.title} /></div>
+
+                                                <div className="item-sel">
+                                                    <div className="item-info">
+                                                        <h4 className='item-title'>{item.title}</h4>
+                                                        <p className='item-option'>{item.sheet.text} / {item.size.sizename} / {item.color.colorname} / {item.add ? item.add.cushion : '선택안함'}
+                                                        </p>
+                                                        <button className='btn-option'><span>옵션변경</span></button>
+                                                        <OptionChange item={item} />
+                                                    </div>
+
+                                                    <div className="count-wrap">
+                                                        <button onClick={() => onItemMinus(item)}><img src="/images/minus.png" alt="빼기아이콘" /></button>
+                                                        <span>{item.qty}</span>
+                                                        <button onClick={() => onItemPlus(item)}><img src="/images/plus.png" alt="더하기아이콘" /></button>
+                                                    </div>
+                                                    <p className='price'>
+                                                        {getItemTotal(item).toLocaleString()}원
+                                                    </p>
+                                                </div>
+                                                
+                                            </div>
+
                                         </div>
-                                    </div>
-                                </div>
-                            ))
+                                    ))
+                                }
+                            </div>
                         )}
                     </div>
 
-                    <div className="total-wrap">
-                        <div className="total-content">
-                            <h4>구매 금액</h4>
-                            <ul>
-                                <li><span>상품금액</span><span>{selectedTotal.toLocaleString()}원</span></li>
-                                <li><span>할인 금액</span><span>{saleTotal.toLocaleString()}</span></li>
-                                <li><span>적립금</span><span>{savePoint.toLocaleString()}원</span></li>
-                                <li><span>배송비</span><span>무료배송</span></li>
-                            </ul>
+                    <div className="total">
+                        <div className="total-wrap">
+                            <div className="total-content">
+                                <h4>구매 금액</h4>
+                                <ul>
+                                    <li><span>상품금액</span><span>{selectedTotal.toLocaleString()}원</span></li>
+                                    <li><span>할인 금액</span><span>{saleTotal.toLocaleString()}</span></li>
+                                    <li><span>적립금</span><span>{savePoint.toLocaleString()}원</span></li>
+                                    <li><span>배송비</span><span>무료배송</span></li>
+                                </ul>
 
-                            <div className="total-price">
-                                <span>총 구매 금액</span>
-                                <strong>
-                                    {finalTotal.toLocaleString()}원
-                                </strong>
+                                <div className="total-price">
+                                    <span>총 구매 금액</span>
+                                    <strong>
+                                        {finalTotal.toLocaleString()}원
+                                    </strong>
+                                </div>
+
+                                <Link to="/payment"><div className='pay-btn'><span>결제하기</span></div></Link>
                             </div>
-
-                            <Link to="/payment"><div className='pay-btn'><span>결제하기</span></div></Link>
                         </div>
                     </div>
 
