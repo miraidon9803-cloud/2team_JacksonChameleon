315ad07532c36dcae284bfb78f2edb03a59bae65
Cheon-U <96rhysc@gmail.com>
2025-11-26 11:52:50 +0900

    a

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
 import { useProductStore } from "../store/ProductStore";
 import "./scss/Payment.scss";
-=======
-import { useProductStore } from "../store/ProductStore";
-import "./scss/Payment.scss";
-import { useNavigate } from "react-router-dom";
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
 import { useAuthStore } from "../store/authStore";
 import Coupon from "../components/Coupon";
 import PaymentDelivery from "../components/PaymentDelivery";
 import PaymentComplete from "../components/PaymentComplete";
 
 const Payment = () => {
-<<<<<<< HEAD
   const navigate = useNavigate();
-=======
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
   /* ------------------ STORE ------------------ */
   const {
     // 요청사항
@@ -53,39 +44,23 @@ const Payment = () => {
     // 주문 리스트
     onAddOrder,
     orderList,
-<<<<<<< HEAD
     directOrderList,
     resetDirectOrder,
     setOrderList,
-=======
-
-    //단독결제
-    checkoutItems,
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
     saveOrder,
     processPayment,
     resetPaymentState,
     onClearCart,
-<<<<<<< HEAD
   } = useProductStore();
 
   const { user } = useAuthStore();
 
   /* ------------------ LOCAL STATE ------------------ */
-=======
-
-    hyphenphone,
-  } = useProductStore();
-
-  const { user } = useAuthStore();
-  const navigate = useNavigate();
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
   const [showCoupon, setShowCoupon] = useState(false);
   const [showDelivery, setShowDelivery] = useState(false);
   const [showComplete, setShowComplete] = useState(false);
   const [inputPoint, setInputPoint] = useState("");
 
-<<<<<<< HEAD
   // 가격 state
   const [selectedTotal, setSelectedTotal] = useState(0);
   const [saleTotal, setSaleTotal] = useState(0);
@@ -104,14 +79,6 @@ const Payment = () => {
       onAddOrder(); // 장바구니 결제
     }
   }, []);
-=======
-  const selectedTotal = getSelectedTotalPrice(); // 상품 금액 총합
-  const saleTotal = getItemSalePrice(); // 즉시할인
-  const couponDiscount = getCouponDiscount(); // 쿠폰 할인
-  const finalPayment = getFinalPayment(); // 최종 결제 금액
-  const savePoint = getsavePoint(); // 예상 적립금
-  const paymentItems = checkoutItems.length > 0 ? checkoutItems : orderList; //단독결제 및 장바구니결제
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
 
   /* ------------------ 가격 자동 재계산 ------------------ */
   useEffect(() => {
@@ -148,7 +115,6 @@ const Payment = () => {
 
   const { resetCheckoutItems } = useProductStore();
 
-<<<<<<< HEAD
   const handleConfirm = () => {
     saveOrder();
     processPayment();
@@ -160,16 +126,6 @@ const Payment = () => {
 
     navigate("/mypage");
   };
-=======
-  useEffect(() => {
-    onAddOrder(); // 장바구니 결제일 때 orderList 저장
-  }, []);
-
-  useEffect(() => {
-    // 페이지 나갈 때 checkoutItems 초기화
-    return () => resetCheckoutItems();
-  }, []);
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
 
   return (
     <div className="checkout-wrap">
@@ -189,19 +145,16 @@ const Payment = () => {
               </div>
               <div className="address">
                 <p>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                   {user?.addnum} {user?.address} {user?.add}
                 </p>
                 <p>{user?.phone}</p>
-=======
-                {user?.address} {user?.add}
-=======
+                <p>
                   {user?.address} {user?.add}
->>>>>>> bc3193a196a9b7f8fe79c5242c06301a3479d592
+
                 </p>
                 <p>{hyphenphone(user?.phone)}</p>
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
+
               </div>
             </div>
 
@@ -237,9 +190,8 @@ const Payment = () => {
                   {reqOptions.map((opt) => (
                     <div
                       key={opt.id}
-                      className={`req-item ${
-                        reqText === opt.label ? "selected" : ""
-                      }`}
+                      className={`req-item ${reqText === opt.label ? "selected" : ""
+                        }`}
                       onClick={(e) => {
                         e.stopPropagation();
                         setIsCustomInput(false);
@@ -270,45 +222,32 @@ const Payment = () => {
             <div className="left-con3 order">
               <p>주문상품</p>
 
-<<<<<<< HEAD
-              {orderList.map((i) => (
-                <div className="order-item" key={i.cartId}>
-                  <div className="item-img">
-                    <img src={i.size?.img} alt="" />
-=======
-              {paymentItems.map((i, idx) => (
-                <div className="order-item" key={i.cartId || idx}>
-                  <div className="item-img">
-                    <img
-                      src={i.size?.img || i.img || "/images/noimage.png"}
-                      alt={i.title}
-                    />
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
-                  </div>
+  {
+    orderList.map((i) => (
+      <div className="order-item" key={i.cartId}>
+        <div className="item-img">
+          <img src={i.size?.img} alt="" />
+              </div>
 
-                  <div className="item-info">
-                    <p className="item-title">{i.title}</p>
-                    <p className="item-option">
-                      {i.sheet?.text} / {i.size?.sizename} /{" "}
-<<<<<<< HEAD
-                      {i.color?.colorname} / {i.add?.cushion || "선택안함"}
-=======
-                      {i.color?.colorname} /{i.add?.cushion || "선택안함"}
->>>>>>> d1847b26c574017e91501b7f28c27be51103f684
-                    </p>
-                    <p className="item-price">
-                      {(
-                        (i.size?.price || 0) + (i.add?.price || 0)
-                      ).toLocaleString("ko-KR")}
-                      원
-                    </p>
-                  </div>
-                </div>
+              <div className="item-info">
+                <p className="item-title">{i.title}</p>
+                <p className="item-option">
+                  {i.sheet?.text} / {i.size?.sizename} /{" "}
+      { i.color?.colorname } / { i.add?.cushion || "선택안함" }
+                    </p >
+  <p className="item-price">
+    {(
+      (i.size?.price || 0) + (i.add?.price || 0)
+    ).toLocaleString("ko-KR")}
+    원
+  </p>
+                  </div >
+                </div >
               ))}
-            </div>
+            </div >
 
-            {/* 적립금 */}
-            <div className="left-con4 acc">
+  {/* 적립금 */ }
+  < div className = "left-con4 acc" >
               <p>적립금 사용</p>
 
               <div className="use-acc">
@@ -333,16 +272,16 @@ const Payment = () => {
                 </button>
               </div>
               <p>보유적립금: {myPoint.toLocaleString("ko-KR")}원</p>
-            </div>
+            </div >
 
-            {/* 쿠폰 */}
-            <div className="left-con5 cupon">
+  {/* 쿠폰 */ }
+  < div className = "left-con5 cupon" >
               <p>쿠폰 사용</p>
               <button onClick={() => setShowCoupon(true)}>쿠폰사용</button>
-            </div>
+            </div >
 
-            {/* 결제수단 */}
-            <div className="left-con6 payment">
+  {/* 결제수단 */ }
+  < div className = "left-con6 payment" >
               <p>결제수단</p>
 
               <form>
@@ -409,94 +348,100 @@ const Payment = () => {
                   </div>
                 </label>
               </form>
-            </div>
-          </div>
+            </div >
+          </div >
 
-          {/* ------------------ RIGHT ------------------ */}
-          <div className="right">
-            <div className="total-wrap">
-              <div className="total-content">
-                <h4>구매 금액</h4>
+  {/* ------------------ RIGHT ------------------ */ }
+  < div className = "right" >
+    <div className="total-wrap">
+      <div className="total-content">
+        <h4>구매 금액</h4>
 
-                <ul>
-                  <li>
-                    <span>상품금액</span>
-                    <span>{selectedTotal.toLocaleString("ko-KR")}원</span>
-                  </li>
+        <ul>
+          <li>
+            <span>상품금액</span>
+            <span>{selectedTotal.toLocaleString("ko-KR")}원</span>
+          </li>
 
-                  <li>
-                    <span>즉시할인</span>
-                    <span>{saleTotal.toLocaleString("ko-KR")}원</span>
-                  </li>
+          <li>
+            <span>즉시할인</span>
+            <span>{saleTotal.toLocaleString("ko-KR")}원</span>
+          </li>
 
-                  <li>
-                    <span>쿠폰할인</span>
-                    <span>{couponDiscount.toLocaleString("ko-KR")}원</span>
-                  </li>
+          <li>
+            <span>쿠폰할인</span>
+            <span>{couponDiscount.toLocaleString("ko-KR")}원</span>
+          </li>
 
-                  <li>
-                    <span>적립금 사용</span>
-                    <span>{usedPoint.toLocaleString("ko-KR")}원</span>
-                  </li>
+          <li>
+            <span>적립금 사용</span>
+            <span>{usedPoint.toLocaleString("ko-KR")}원</span>
+          </li>
 
-                  <li>
-                    <span>배송비</span>
-                    <span>무료배송</span>
-                  </li>
-                </ul>
+          <li>
+            <span>배송비</span>
+            <span>무료배송</span>
+          </li>
+        </ul>
 
-                <h4>적립혜택</h4>
-                <div className="cash">
-                  <p>적립금</p>
-                  <p>{savePoint.toLocaleString("ko-KR")}원</p>
-                </div>
+        <h4>적립혜택</h4>
+        <div className="cash">
+          <p>적립금</p>
+          <p>{savePoint.toLocaleString("ko-KR")}원</p>
+        </div>
 
-                <div className="total-price">
-                  <span>총 결제 금액</span>
-                  <strong>{finalPayment.toLocaleString("ko-KR")}원</strong>
-                </div>
-                <button
-                  className="pay-btn"
-                  onClick={() => {
+        <div className="total-price">
+          <span>총 결제 금액</span>
+          <strong>{finalPayment.toLocaleString("ko-KR")}원</strong>
+        </div>
+        <button
+          className="pay-btn"
+          onClick={() => {
 <<<<<<< HEAD
-                    if (directOrderList.length === 0) {
-                      onClearCart();
-                    }
+            if (directOrderList.length === 0) {
+              onClearCart();
+            }
 
-                    setShowComplete(true);
+            setShowComplete(true);
 =======
                     handleConfirm(); // 결제 처리
 >>>>>>> d1847b26c574017e91501b7f28c27be51103f684
-                  }}
-                >
-                  결제하기
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+          }}
+        >
+          결제하기
+        </button>
+      </div>
+    </div>
+          </div >
+        </div >
 
-        {/* 팝업 */}
-        {showCoupon && <Coupon onClose={() => setShowCoupon(false)} />}
-        {showDelivery && (
-          <PaymentDelivery onClose={() => setShowDelivery(false)} />
-        )}
-        {showComplete && (
+  {/* 팝업 */ }
+{ showCoupon && <Coupon onClose={() => setShowCoupon(false)} /> }
+{
+  showDelivery && (
+    <PaymentDelivery onClose={() => setShowDelivery(false)} />
+  )
+}
+{
+  showComplete && (
 <<<<<<< HEAD
-          <PaymentComplete
-            onConfirm={handleConfirm}
-            onClose={() => setShowComplete(false)}
-          />
+    <PaymentComplete
+      onConfirm={handleConfirm}
+      onClose={() => setShowComplete(false)}
+    />
 =======
           <PaymentComplete onClose={() => setShowComplete(false)} />
 >>>>>>> d1847b26c574017e91501b7f28c27be51103f684
-        )}
+  )
+}
 
-        {(showCoupon || showDelivery || showComplete) && (
-          <div className="popup-overlay"></div>
-        )}
-      </div>
-    </div>
+{
+  (showCoupon || showDelivery || showComplete) && (
+    <div className="popup-overlay"></div>
+  )
+}
+      </div >
+    </div >
   );
 };
 
