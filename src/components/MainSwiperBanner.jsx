75b7f72956ade4633e4204aef0caa4123ae8332a
alsrl980409 <miraidon9803@gmail.com>
2025-11-26 11:08:47 +0900

    스토어글씨수정

--- src/components/MainSwiperBanner.jsx
+++ src/components/MainSwiperBanner.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 import "./scss/MainSwiperBanner.scss";
-import 'animate.css';
-
+import "animate.css";
 
 const MainSwiperBanner = ({ title, subTitle, frontImg, backImg }) => {
-    return (
-        <section className="main-swiper">
-            <div className="text-box">
-                <p className='animate__animated animate__fadeInDown'>{subTitle}</p>
-                <h2 className='animate__animated animate__fadeInDown'>{title}</h2>
-                <button className='animate__animated animate__fadeInDown'>SHOP NOW</button>
-            </div>
+  return (
+    <section className="main-swiper">
+      <div className="text-box">
+        <p className="animate__animated animate__fadeInDown">{subTitle}</p>
+        <h2 className="animate__animated animate__fadeInDown">{title}</h2>
+        <button className="animate__animated animate__fadeInDown">
+          SHOP NOW
+        </button>
+      </div>
 
-            <div className="img-box">
-                <img className='front wow animate__fadeInBottomLeft' data-wow-delay="1s" src={frontImg} alt="앞제품이미지" />
-                <img className='back' src={backImg} alt="뒤제품이미지" />
-            </div>
-        </section>
-    )
-}
+      <div className="img-box">
+        <img
+          className="front wow animate__fadeInBottomLeft"
+          data-wow-delay="1s"
+          src={frontImg}
+          alt="앞제품이미지"
+        />
+        <img className="back" src={backImg} alt="뒤제품이미지" />
+      </div>
+    </section>
+  );
+};
 
-export default MainSwiperBanner
+export default MainSwiperBanner;
